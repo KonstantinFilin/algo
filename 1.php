0a6d9486ae06634c11b832f273672eea853fb0f9
Konstantin S. Filin <info@kfilin.ru>
2020-12-25 00:03:57 +0300

    Gcd algorithm

---
+++ 1.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+$a = new \Algo\Nums\Gcd(99, 63);
+
+var_dump($a->get());
---
+++ src/Nums/Gcd.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Algo\Nums;
+
+class Gcd {
+    /**
+     *
+     * @var int
+     */
+    public $a;
+
+    /**
+     *
+     * @var int
+     */
+    public $b;
+
+    private $log = [
+        [ 'a', 'b', 'calculation']
+    ];
+
+    public function __construct(int $a, int $b) {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    private function calc(int $a, int $b): int {
+echo $a . ":" . $b . PHP_EOL;
+        if ($a == $b) {
+            $this->log[] = [
+                $a, $b, "Stop"
+            ];
+            return $a;
+        }
+
+        if ($a > $b) {
+            $this->log[] = [
+                $a, $b, "a = a - b (" . $a . " - " . $b . " = " . ( $a - $b ) . ")"
+            ];
+            return $this->calc($a - $b, $b);
+        }
+
+        return $this->calc($a, $b - $a);
+    }
+
+    public function get(): int {
+        return $this->calc($this->a, $this->b);
+    }
+
+    function getLog(): array {
+        return $this->log;
+    }
+}
